5a43400f1fbc54c7489df8d85bd90e6bfc172ece
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-03-01 17:45:33 +0400

    git

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function addFamilyMember() {
   const budget = parseInt(document.getElementById('budget').value);
   const spending = parseInt(document.getElementById('spending').value);
 
-  for(let i = 0; i < familyMembers.length + 1; i++){
-    if(isNaN(budget && spending)){
-      alert("Enter the Inputs!");
-      return 0;
-    }
-  }
+  // for(let i = 0; i < familyMembers.length + 1; i++){
+  //   if(isNaN(budget)){
+  //     alert("Enter the Inputs!");
+  //     return 0;
+  //   }
+  // }
 
   var totalBudget = 0;
   for(let j = 0; i < familyMembers.length; j++){
@@ -59,10 +59,10 @@ function calculateTotal() {
   let eduPayment = educationPayment / 12;
   const total = utilityBills + eduPayment + otherPayment;
   // Update the total display
-  if(total == 0){
-    alert("Enter the spends!");
-    return 0;
-  }
+  // if(total == 0){
+  //   alert("Enter the spends!");
+  //   return 0;
+  // }
 
   document.getElementById("total").innerHTML = `Total: $${Math.floor(total)}`;
   document.getElementById("budgetlast").innerHTML = `Total: $${totalBudget}`;

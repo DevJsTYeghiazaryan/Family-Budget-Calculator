cf8a5731e0d2375ea6e1f1cbbd871387e39e3f2c
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-03-02 20:36:01 +0400

    Modifie script

--- script.js
+++ script.js
@@ -6,16 +6,11 @@ function addFamilyMember() {
   const budget = parseInt(document.getElementById('budget').value);
   const spending = parseInt(document.getElementById('spending').value);
 
-  // for(let i = 0; i < familyMembers.length + 1; i++){
-  //   if(isNaN(budget)){
-  //     alert("Enter the Inputs!");
-  //     return 0;
-  //   }
-  // }
-
-  var totalBudget = 0;
-  for(let j = 0; i < familyMembers.length; j++){
-      totalBudget += familyMembers[j].budget
+  for(let i = 0; i < familyMembers.length + 1; i++){
+    if(isNaN(budget && spending)){
+      alert("Enter the Inputs!");
+      return 0;
+    }
   }
 
 
@@ -49,20 +44,20 @@ function addFamilyMember() {
 
 
 function calculateTotal() {
-  // Get the input values
   const utilityBills = parseInt(document.getElementById("utility-bills").value) || 0;
   const educationPayment = parseInt(document.getElementById("education-payment").value) || 0;
   const otherPayment = parseInt(document.getElementById("other-payment").value) || 0;
 
   
-  // Calculate the total
+
   let eduPayment = educationPayment / 12;
   const total = utilityBills + eduPayment + otherPayment;
-  // Update the total display
-  // if(total == 0){
-  //   alert("Enter the spends!");
-  //   return 0;
-  // }
+
+  
+  if(total == 0){
+    alert("Enter the spends!");
+    return 0;
+  }
 
   document.getElementById("total").innerHTML = `Total: $${Math.floor(total)}`;
   document.getElementById("budgetlast").innerHTML = `Total: $${totalBudget}`;
